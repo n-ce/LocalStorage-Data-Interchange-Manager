7c7fe12beceb473e4eb5f3d7f05b5964abca7dc3
Animesh <69345507+n-ce@users.noreply.github.com>
2022-08-20 20:21:34 +0530

    implement array.forEach()

--- script.js
+++ script.js
@@ -2,25 +2,46 @@ const input = document.getElementsByTagName('input');
 const display = document.getElementsByTagName('article')[0];
 const table = document.querySelector('tbody');
 
+
+let found = false;
 let arr = [];
 let obj;
 
-let Push = () => {
-  if (input[0].value !== '' && input[1].value !== '') {
-    obj = {
-      val1: input[0].value,
-      val2: input[1].value
-    };
-    table.innerHTML += `<tr><td>${obj.val1}</td><td>${obj.val2}</td></tr>`;
-    arr.push(obj);
+let update = (v1, v2, v3) => {
+  if (v1 != '' && found == false) {
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    td1.innerText = v1;
+    td2.innerText = v2;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr)
+    arr.push(v3);
   }
+}
+
+let Push = () => {
+  obj = {
+    val1: input[0].value,
+    val2: input[1].value
+  };
+
+  arr.forEach((e, i) => {
+    if (e.val1 === obj.val1) {
+      document.getElementsByTagName('td')[(i*2)+1].innerText = e.val2 = obj.val2;
+      found = true;
+    }
+    else { found = false; }
+  });
+  update(obj.val1, obj.val2, obj);
 }
 
 let Export = () => {
   if (obj != '') {
     const textToBLOB = new Blob([JSON.stringify(arr)], { type: 'application/json' });
     let newLink = document.createElement("a");
-    newLink.download = `data_${Math.floor(Math.random() * 999)}.json`;
+    newLink.download = `LSDI_${Math.floor(Math.random() * 999)}.json`;
     if (window.webkitURL != null) {
       newLink.href = window.webkitURL.createObjectURL(textToBLOB);
     }
@@ -32,20 +53,10 @@ let Export = () => {
     newLink.click();
   }
 }
+
 let Import = async file => {
   arr = arr.concat(JSON.parse(await file.text()));
   for (const x of arr) {
     table.innerHTML = `<tr><td>${x.val1}</td><td>${x.val2}</td></tr>`;
   }
-}
-
-
- // you can loop through array in which you are pushing your object.
-// In first input whenever you write the value which is already in array and writting second value change then call this function.
-let ChangeVaL2=(val1,val2)=>{
-  arr.forEach((element)=>{
-    if(element.val1===val1){
-      element.val2=val2;
-    }
-  });
 }
