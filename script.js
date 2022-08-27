becabb8a8e43ec59cc0457275cfc81fbc9722b6d
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-08-27 23:14:52 +0530

    Fix #1, Fix #2

--- script.js
+++ script.js
@@ -2,17 +2,39 @@ const input = document.getElementsByTagName('input');
 const display = document.getElementsByTagName('article')[0];
 const table = document.querySelector('tbody');
 
+
+let n = 0;
 let arr = [];
 let obj;
 
+let update = (v1, v2, v3) => {
+  if (n == arr.length) {
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    td1.innerText = v1;
+    td2.innerText = v2;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr)
+    arr.push(v3);
+    localStorage.setItem('data', JSON.stringify(arr))
+  }
+}
+
 let Push = () => {
-  if (input[0].value !== '' && input[1].value !== '') {
+  if (input[0].value != '') {
     obj = {
       val1: input[0].value,
       val2: input[1].value
     };
-    table.innerHTML += `<tr><td>${obj.val1}</td><td>${obj.val2}</td></tr>`;
-    arr.push(obj);
+    n = 0;
+    arr.forEach((e, i) => {
+      e.val1 == obj.val1 ?
+        document.getElementsByTagName('td')[(i * 2) + 1].innerText = e.val2 = obj.val2 :
+        n++;
+    });
+    update(obj.val1, obj.val2, obj);
   }
 }
 
@@ -20,7 +42,7 @@ let Export = () => {
   if (obj != '') {
     const textToBLOB = new Blob([JSON.stringify(arr)], { type: 'application/json' });
     let newLink = document.createElement("a");
-    newLink.download = `data_${Math.floor(Math.random() * 999)}.json`;
+    newLink.download = `LSDI_${Math.floor(Math.random() * 999)}.json`;
     if (window.webkitURL != null) {
       newLink.href = window.webkitURL.createObjectURL(textToBLOB);
     }
@@ -33,9 +55,18 @@ let Export = () => {
   }
 }
 
+let load = () => {
+  n = arr.length;
+  for (const x of arr) {
+    table.innerHTML += `<tr><td>${x.val1}</td><td>${x.val2}</td></tr>`;
+  }
+}
 let Import = async file => {
   arr = arr.concat(JSON.parse(await file.text()));
-  for (const x of arr) {
-    table.innerHTML = `<tr><td>${x.val1}</td><td>${x.val2}</td></tr>`;
-  }
+  load();
+}
+
+if (localStorage.getItem('data') != null) {
+  arr = JSON.parse(localStorage.getItem('data'));
+  load();
 }
